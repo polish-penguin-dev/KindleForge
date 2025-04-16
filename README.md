c1db682999dbdb36c59e99ceeb4ecf79147645f5
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-04-16 21:27:40 +0100

    Actually Make A Registry

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 - KindleForge Is A Simple Appstore For Jailbroken Kindles.
 - It Is A Simple WAF, And Works Similarly To Mesquito.
+- This Uses And Relies On *A LOT* Of Scriptlets And Icons From The Kindle Modding Community! 
+- Proper Credit Is Given, But Always DM Me If You Want Something Taking Down (Or, Adding!)
 
 ![UI Example](https://github.com/user-attachments/assets/44e8fe42-9629-4298-8f0d-1b224d360ca4)
 
--- kindleforge/script.js
+++ kindleforge/script.js
@@ -1,36 +1,33 @@
-//This JS Is Uglified Substantially For It To Work On Kindle.C
+//This JS Is Uglified Substantially For It To Work On Kindle.
 
 var apps = [
     {
       name: "KOReader",
       author: "Hans-Werner",
-      description: "An E-book Reader Application",
-      downloads: 12420
+      description: "An E-book Reader Application"
     },
     {
       name: "kTerm",
       author: "Bartek Fabiszewski",
-      description: "GTK+ Terminal Emulator For Kindle",
-      downloads: 8930
+      description: "GTK+ Terminal Emulator For Kindle"
     },
     {
       name: "KindleFetch",
       author: "Justrals",
-      description: "Download Books From Anna's Archive",
-      downloads: 15640
+      description: "Download Books From Annas Archive"
     }
 ];
   
 var icons = {
-    book: '<svg class="icon" viewBox="0 0 24 24">' +
-            '<path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>' +
-            '<path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>' +
-          '</svg>',
-    download: '<svg class="icon" viewBox="0 0 24 24">' +
-                '<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>' +
-                '<polyline points="7 10 12 15 17 10"></polyline>' +
-                '<line x1="12" y1="15" x2="12" y2="3"></line>' +
-              '</svg>'
+    book: "<svg class='icon' viewBox='0 0 24 24'>" +
+            "<path d='M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z'></path>" +
+            "<path d='M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z'></path>" +
+          "</svg>",
+    download: "<svg class='icon' viewBox='0 0 24 24'>" +
+                "<path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'></path>" +
+                "<polyline points='7 10 12 15 17 10'></polyline>" +
+                "<line x1='12' y1='15' x2='12' y2='3'></line>" +
+              "</svg>"
 };
   
 function formatNumber(num) {
@@ -38,20 +35,20 @@ function formatNumber(num) {
 }
   
 function createAppCard(app) {
-  var html = '';
-  html += '<article class="app-card">';
-  html += '  <div class="app-header">';
-  html += '    <div class="app-title-box">';
-  html += '      <h2 class="app-title">' + app.name + '</h2>';
-  html += '      <p class="app-author">by ' + app.author + '</p>';
-  html += '    </div>';
-  html += '  </div>';
-  html += '  <p class="app-description">' + app.description + '</p>';
-  html += '  <button class="install-button">';
+  var html = "";
+  html += "<article class='app-card'>";
+  html += "  <div class='app-header'>";
+  html += "    <div class='app-title-box'>";
+  html += "      <h2 class='app-title'>" + app.name + "</h2>";
+  html += "      <p class='app-author'>by " + app.author + "</p>";
+  html += "    </div>";
+  html += "  </div>";
+  html += "  <p class='app-description'>" + app.description + "</p>";
+  html += "  <button class='install-button'>";
   html +=      icons.download;
-  html += '    Install Application';
-  html += '  </button>';
-  html += '</article>';
+  html += "    Install Application";
+  html += "  </button>";
+  html += "</article>";
   return html;
 }
 
@@ -59,16 +56,14 @@ function createAppCard(app) {
   
 function renderApps() {
     var appList = document.getElementById("app-list");
-    var html = '';
+    var html = "";
     for (var i = 0; i < apps.length; i++) {
       html += createAppCard(apps[i]);
     }
     appList.innerHTML = html;
-    
-    // Add click event listeners for each install button
+
     var installButtons = document.querySelectorAll(".install-button");
     for (var i = 0; i < installButtons.length; i++) {
-      // Create a closure to capture the current index
       (function(index) {
         installButtons[index].addEventListener("click", function() {
           alert("Installing " + apps[index].name + "...");
