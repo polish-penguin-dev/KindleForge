62fa1a17f8a94620659f74c905d469e05c1c1bd1
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-04-17 17:38:21 +0100

    Shell Execution!
    (You heard that right)

--- kindleforge/script.js
+++ kindleforge/script.js
@@ -1,24 +1,31 @@
 //This JS Is Uglified Substantially For It To Work On Kindle.
 
-var apps = [
-    {
-      name: "KOReader",
-      author: "Hans-Werner",
-      description: "An E-book Reader Application"
-    },
-    {
-      name: "kTerm",
-      author: "Bartek Fabiszewski",
-      description: "GTK+ Terminal Emulator For Kindle"
-    },
-    {
-      name: "KindleFetch",
-      author: "Justrals",
-      description: "Download Books From Annas Archive"
+var apps = null;
+
+function fetchApps(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        try {
+          apps = JSON.parse(xhr.responseText);
+          onAppsLoaded(); 
+        } catch (e) {
+          console.error("Failed to parse JSON:", e);
+        }
+      } else {
+        console.error("Request failed with status " + xhr.status);
+      }
     }
-];
-  
-var icons = {
+  };
+
+  xhr.send();
+}
+
+function onAppsLoaded() {
+  var icons = {
     book: "<svg class='icon' viewBox='0 0 24 24'>" +
             "<path d='M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z'></path>" +
             "<path d='M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z'></path>" +
@@ -28,33 +35,29 @@ var icons = {
                 "<polyline points='7 10 12 15 17 10'></polyline>" +
                 "<line x1='12' y1='15' x2='12' y2='3'></line>" +
               "</svg>"
-};
+  };
   
-function formatNumber(num) {
-    return num.toLocaleString();
-}
-  
-function createAppCard(app) {
-  var html = "";
-  html += "<article class='app-card'>";
-  html += "  <div class='app-header'>";
-  html += "    <div class='app-title-box'>";
-  html += "      <h2 class='app-title'>" + app.name + "</h2>";
-  html += "      <p class='app-author'>by " + app.author + "</p>";
-  html += "    </div>";
-  html += "  </div>";
-  html += "  <p class='app-description'>" + app.description + "</p>";
-  html += "  <button class='install-button'>";
-  html +=      icons.download;
-  html += "    Install Application";
-  html += "  </button>";
-  html += "</article>";
-  return html;
-}
+  function createAppCard(app) {
+    var html = "";
+    html += "<article class='app-card'>";
+    html += "  <div class='app-header'>";
+    html += "    <div class='app-title-box'>";
+    html += "      <h2 class='app-title'>" + app.name + "</h2>";
+    html += "      <p class='app-author'>by " + app.author + "</p>";
+    html += "    </div>";
+    html += "  </div>";
+    html += "  <p class='app-description'>" + app.description + "</p>";
+    html += "  <button class='install-button'>";
+    html +=      icons.download;
+    html += "    Install Application";
+    html += "  </button>";
+    html += "</article>";
+    return html;
+  };
 
 
   
-function renderApps() {
+  function renderApps() {
     var appList = document.getElementById("app-list");
     var html = "";
     for (var i = 0; i < apps.length; i++) {
@@ -66,13 +69,17 @@ function renderApps() {
     for (var i = 0; i < installButtons.length; i++) {
       (function(index) {
         installButtons[index].addEventListener("click", function() {
-          alert("Installing " + apps[index].name + "...");
+          //Trigger Installation Code
+          kindle.messaging.sendMessage("com.lab126.pillow", "customDialog", { name: "../../../../../../mnt/us/kindleforge/dialoger", clientParams: { show: true, jsEnabled: false } });
         });
       })(i);
     }
-}
-  
+  };
+
+  renderApps();
+};  
+
 document.addEventListener("DOMContentLoaded", function() {
-    renderApps();
+    fetchApps("https://raw.githubusercontent.com/polish-penguin-dev/KindleForge/refs/heads/master/registry.json"); //Fetch App Registry
     document.getElementById("status").innerHTML = "JS Working!";
 });
